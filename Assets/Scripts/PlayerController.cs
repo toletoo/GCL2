4604153ee4f26284452f71ca3a2d2d5de6c134c9
toletoo <yomamanbruh@gmail.com>
2026-07-01 11:25:51 +0800

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
     public float walkSpeed = 10f;
+    public bool isRight = false;
+    private float direction;
     private Rigidbody2D rb;
     private SpriteRenderer spriteRenderer;
     private Animator anim;
@@ -18,12 +20,19 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.D)) 
+        direction = isRight ? 1.0f : -1.0f;
+        if (Input.GetAxisRaw("Horizontal") > 0) 
         {
-            
-        } else if (Input.GetKeyDown(KeyCode.A))
+            isRight = true;
+            rb.linearVelocityX = walkSpeed * direction;
+        } else if (Input.GetAxisRaw("Horizontal") < 0)
         {
-
+            isRight = false;
+            rb.linearVelocityX = walkSpeed * direction;
+        }
+        else
+        {
+            rb.linearVelocityX = 0;
         }
     }
 }
