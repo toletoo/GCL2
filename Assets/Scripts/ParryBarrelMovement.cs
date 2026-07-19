a4e5a03cfb51d19ee3d14f8edd6d63d2b0502cb8
toletoo <yomamanbruh@gmail.com>
2026-07-19 18:39:45 +0800

    Update ParryBarrelMovement.cs

--- Assets/Scripts/ParryBarrelMovement.cs
+++ Assets/Scripts/ParryBarrelMovement.cs
@@ -2,10 +2,13 @@ using UnityEngine;
 
 public class ParryBarrelMovement : MonoBehaviour
 {
+    private Transform playerPosition;
+    private Rigidbody2D rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        playerPosition = FindAnyObjectByType<PlayerController>().transform;
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -13,4 +16,5 @@ public class ParryBarrelMovement : MonoBehaviour
     {
         
     }
+    
 }
