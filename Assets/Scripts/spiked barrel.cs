2a236fa0b5280a9a5584c03927a86999fd5ed86c
destu-pidus <sebastinabruuuh@gmail.com>
2026-07-21 19:30:33 +0800

    sebastian adds spikey barrel
    breaks shield and hammer

---
+++ Assets/Scripts/spiked barrel.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using System.Collections;
+
+public class spikedbarrel : MonoBehaviour
+{
+
+    private LevelManager levelManager;
+    [SerializeField] GameObject barrel;
+    [SerializeField] float barrelSpawnCooldown = 2f;
+    private bool canSpawn = false;
+    public bool isStun = false;
+
+    // for debugging purposes
+    [SerializeField] int maxSpawnCount = 1;
+    private int currentSpawnCount = 0;
+    private Animator monkeyAnim;
+
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        levelManager = FindFirstObjectByType<LevelManager>();
+        monkeyAnim = GameObject.FindGameObjectWithTag("Monkey").GetComponent<Animator>();
+        canSpawn = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (levelManager.currentGameState != GameState.Playing) return;//added cant spawn when player is dead
+        SpawnBarrelCo();
+    }
+
+    void SpawnBarrelCo()
+    {
+        if (levelManager.gameStarted && canSpawn && !isStun)
+        {
+            StartCoroutine(SpawnBarrel());
+        }
+    }
+
+    IEnumerator SpawnBarrel()
+    {
+        monkeyAnim.SetTrigger("isThrowing");
+        Instantiate(barrel, gameObject.transform.position, Quaternion.identity); // spawn barrels every x seconds
+        currentSpawnCount++;
+        canSpawn = false;
+        yield return new WaitForSeconds(barrelSpawnCooldown);
+        if (currentSpawnCount < maxSpawnCount)
+        {
+            canSpawn = true;
+        }
+    }
+
+    public IEnumerator Stun()
+    {
+        monkeyAnim.SetBool("isStun", true);
+        //Cant spawn barrel for set time
+        isStun = true;
+        yield return new WaitForSeconds(2f);
+        monkeyAnim.SetBool("isStun", false);
+        isStun = false;
+    }
+}
